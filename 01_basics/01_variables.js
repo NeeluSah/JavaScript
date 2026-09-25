473557c89e110b4fb9049fcba9fd2d3c2bd0c6b8
NeeluSah <neelusah1231@gmail.com>
2026-09-25 07:29:54 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 13389
+let accountEmail = "Neelu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 // Not allowed 
+
+accountEmail = "he@ks.com"
+accountPassword = "2323423"
+accountCity ="Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
